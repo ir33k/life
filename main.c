0311529177653d9e3d5d9dd2f8f0eb368335faea
irek <irek@gabr.pl>
2023-05-08 11:56:29 +0200

    Avoid slow division

--- main.c
+++ main.c
@@ -9,16 +9,18 @@
 static u8
 cell_neighbours(i32 x, i32 y)
 {
+	static const i8 map_y[8] = {-1, -1, -1, +0, +0, +1, +1, +1};
+	static const i8 map_x[8] = {-1, +0, +1, -1, +1, -1, +0, +1};
 	u8 res = 0;		/* Result, number of neighbours */
 	i32 i, dx, dy;		/* Index and delta coordinates */
-	for (i=0; i<9; i++) {
-		dy = y + (i/3 -1);
-		dx = x + (i%3 -1);
+	for (i=0; i<8; i++) {
+		dy = y + map_y[i];
+		dx = x + map_x[i];
 		if (dy < 0 || dy >= SIZ_Y) continue;
 		if (dx < 0 || dx >= SIZ_X) continue;
 		res += s_board[s_i][dy][dx];
 	}
-	return res - s_board[s_i][y][x];
+	return res;
 }
 
 static void
